caf2b7cf2c24cc1710a62f322940886cccd6b87d
Ben Lam <benlam@microsoft.com>
2017-09-19 10:23:19 +0800

    '170919'

--- src/main/java/ms/kenchen/Calculator/CalculatorService.java
+++ src/main/java/ms/kenchen/Calculator/CalculatorService.java
@@ -13,7 +13,7 @@ public class CalculatorService {
     @Path("ping")
     @Produces(MediaType.TEXT_PLAIN)
     public String ping() {
-        return "Welcome to Java Web App on Linux v6!!! This is updated!\n" + new Date().toString();
+        return "Welcome to Java Web API on Container v7!\n" + new Date().toString();
     }
 
     @GET
