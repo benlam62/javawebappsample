7cfc74adb0da2492b85f4ecee93a0ab482184a0f
benlam62 <benlam62@gmail.com>
2017-09-26 10:06:25 +0800

    '170926'

--- src/main/java/ms/kenchen/Calculator/CalculatorService.java
+++ src/main/java/ms/kenchen/Calculator/CalculatorService.java
@@ -25,7 +25,7 @@ public class CalculatorService {
     }
 
     @GET
-    @Path("sub")
+    @Path("sub")­
     @Produces(MediaType.APPLICATION_JSON)
     public CalculatorResponse Sub(@QueryParam("x") int x, @QueryParam("y") int y) {
         return new CalculatorResponse(x, y, x - y);
@@ -49,6 +49,6 @@ public class CalculatorService {
     @Path("max")
     @Produces(MediaType.APPLICATION_JSON)
     public CalculatorResponse Max(@QueryParam("x") int x, @QueryParam("y") int y) {
-        return new CalculatorResponse(x, y, Math.min(x, y));
+        return new CalculatorResponse(x, y, Math.max(x, y));
     }
 }
